aa03d385d321de9fad547b99bf83403cc8f62c0b
Alain Giros <Alain.Giros@cnes.fr>
2020-01-24 13:54:20 +0100

    split user directories definition dictionary by OS

--- resto_client_tests/tu/generic/utest_user_dirs.py
+++ resto_client_tests/tu/generic/utest_user_dirs.py
@@ -17,7 +17,7 @@ import shutil
 import unittest
 
 from resto_client.generic.user_dirs import (user_config_dir, user_download_dir, user_dir,
-                                            MSWINDOWS, KNOWN_USER_DIRS)
+                                            MSWINDOWS, USER_DIRS_XDG, USER_DIRS_WINDOWS)
 
 
 class UTestUserConfigDir(unittest.TestCase):
@@ -86,19 +86,27 @@ class UTestUserDir(unittest.TestCase):
         """
         Unit test of user_dir() in nominal cases
         """
-        for dir_key in KNOWN_USER_DIRS.keys():
-            # Warning: follwing lists depend on the account under which tests are run
-            if MSWINDOWS:
-                defined_dirs = ['Contacts', 'Downloads', 'Libraries', 'Links', 'LocalAppDataLow',
-                                'RoamingTiles', 'SavedGames', 'SavedSearches']
-            else:
-                defined_dirs = ['Desktop', 'Documents', 'Downloads', 'Music', 'Pictures',
-                                'PublicShare', 'Templates', 'Videos']
-            if dir_key in defined_dirs:
-                user_dir_path = user_dir(dir_key)
-                self.assertTrue(user_dir_path.is_dir())
-            else:
-                self.assertRaises(FileNotFoundError)
+        if MSWINDOWS:
+            # Warning: following lists depend on the account under which tests are run
+            defined_dirs = ['Contacts', 'Downloads', 'Libraries', 'Links', 'LocalAppDataLow',
+                            'RoamingTiles', 'SavedGames', 'SavedSearches']
+            for dir_key in USER_DIRS_WINDOWS:
+                if dir_key in defined_dirs:
+                    user_dir_path = user_dir(dir_key)
+                    self.assertTrue(user_dir_path.is_dir())
+                else:
+                    self.assertRaises(FileNotFoundError)
+
+        else:
+            # Warning: following lists depend on the account under which tests are run
+            defined_dirs = ['Desktop', 'Documents', 'Downloads', 'Music', 'Pictures',
+                            'PublicShare', 'Templates', 'Videos']
+            for dir_key in USER_DIRS_XDG:
+                if dir_key in defined_dirs:
+                    user_dir_path = user_dir(dir_key)
+                    self.assertTrue(user_dir_path.is_dir())
+                else:
+                    self.assertRaises(FileNotFoundError)
 
     def test_d_user_dir(self) -> None:
         """
@@ -107,10 +115,3 @@ class UTestUserDir(unittest.TestCase):
         # Unexisting directory key, on any system
         with self.assertRaises(IndexError):
             _ = user_dir('fake')
-
-        # Non existing directory on the current system
-        with self.assertRaises(NotADirectoryError):
-            if MSWINDOWS:
-                _ = user_dir('PublicShare')
-            else:
-                _ = user_dir('Favorites')
