40fc17cdb6879c0b6d4fedbe4e7826548c5d520c
Alain Giros <Alain.Giros@cnes.fr>
2020-01-25 15:07:29 +0100

    adapted default download directory to modified user_dirs module

--- resto_client/settings/resto_client_config.py
+++ resto_client/settings/resto_client_config.py
@@ -12,9 +12,11 @@
    or implied. See the License for the specific language governing permissions and
    limitations under the License.
 """
-from resto_client.generic.user_dirs import user_config_dir
+from resto_client.generic.user_dirs import user_config_dir, user_download_dir
 
 RESTO_CLIENT_APP_NAME = 'resto_client'
 RESTO_CLIENT_APP_AUTHOR = 'CNES'
 RESTO_CLIENT_CONFIG_DIR = user_config_dir(app_author=RESTO_CLIENT_APP_AUTHOR,
                                           app_name=RESTO_CLIENT_APP_NAME)
+
+RESTO_CLIENT_DEFAULT_DOWNLOAD_DIR = user_download_dir('resto_client_files', ensure_exists=True)
