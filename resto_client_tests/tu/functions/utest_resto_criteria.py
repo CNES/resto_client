279cbc5da8dfdb3180765eb39b104ac15839b30f
Alain Giros <Alain.Giros@cnes.fr>
2020-01-27 16:32:26 +0100

    split resto_criteria in 2 modules

--- resto_client_tests/tu/functions/utest_resto_criteria.py
+++ resto_client_tests/tu/functions/utest_resto_criteria.py
@@ -15,7 +15,8 @@
 import unittest
 
 from resto_client.base_exceptions import RestoClientUserError
-from resto_client.entities.resto_criteria import RestoCriteria, test_criterion
+from resto_client.entities.resto_criteria import RestoCriteria
+from resto_client.entities.resto_criteria_definition import test_criterion
 from resto_client.generic.basic_types import GeometryWKT
 from resto_client.services.resto_server import RestoServer
 
