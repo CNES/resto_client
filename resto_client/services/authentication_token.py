b8da1607de4bf853de49c9a35eaa3dea361ae9a1
Alain Giros <Alain.Giros@cnes.fr>
2020-02-13 11:04:18 +0100

    introduced _renew() and _force_renew() and used them.

--- resto_client/services/authentication_token.py
+++ resto_client/services/authentication_token.py
@@ -32,12 +32,12 @@ class AuthenticationToken():
         """
         self.parent_credentials = parent_credentials
         self._token_value: Optional[str] = None
-        self._unvalidated_token = True
+        self._should_be_valid = False
         self._being_renewed = False
 
     def __str__(self) -> str:
-        result_fmt = 'Token status :\nunvalidated: {}  being_renewed: {} value: {}'
-        return result_fmt.format(self._unvalidated_token, self._being_renewed, self._token_value)
+        result_fmt = 'Token status :\nshould_be_valid: {}  being_renewed: {} value: {}'
+        return result_fmt.format(self._should_be_valid, self._being_renewed, self._token_value)
 
     @property
     def token_value(self) -> Optional[str]:
@@ -47,10 +47,9 @@ class AuthenticationToken():
         if self._being_renewed:
             return None
         if self._token_value is not None:
-            if self._unvalidated_token:
-                self._unvalidated_token = not self.parent_credentials.check_token(self._token_value)
-                if self._unvalidated_token:
-                    self._renew()
+            if not self._should_be_valid:
+                self._should_be_valid = self.parent_credentials.check_token(self._token_value)
+                self._renew()
         return self._token_value
 
     @token_value.setter
@@ -58,21 +57,27 @@ class AuthenticationToken():
         """
         Set the token value.
 
-        :param token_value: If None, reset the token. If different from current value,
-                            validate the current
-                                   token or renew it if it is invalid.
+        :param token_value: If None, reset the token. Otherwise store the token assuming that it is
+                            valid.
         """
-        self._unvalidated_token = token_value is None or token_value != self.token_value
         self._token_value = token_value
+        self._should_be_valid = token_value is not None
 
     def _renew(self) -> None:
         """
-        Renew the current token by getting a new value from the server
+        Renew the current token if it may be invalid, by getting a new value from the server
         """
-        self._being_renewed = True
-        self._token_value = self.parent_credentials.get_token()
-        self._being_renewed = False
-        self._unvalidated_token = self._token_value is None
+        if not self._should_be_valid:
+            self._force_renew()
+
+    def _force_renew(self) -> None:
+        """
+        Renew the current token unconditionaly, by getting a new value from the server
+        """
+        if not self._being_renewed:
+            self._being_renewed = True
+            self.token_value = self.parent_credentials.get_token()
+            self._being_renewed = False
 
     def get_authorization_header(self,
                                  authentication_required: bool,
@@ -88,12 +93,12 @@ class AuthenticationToken():
         """
         authorization_header = {}
         if authentication_required:
-            if self._unvalidated_token:
-                self.token_value = 'A fake token value to trigger get_token()'
+            self._renew()
         else:
             if username_defined:
-                if self._unvalidated_token:
-                    self.token_value = 'A fake token value to trigger get_token()'
-        if self.token_value is not None:
-            authorization_header['Authorization'] = 'Bearer ' + self.token_value
+                self._renew()
+        # Get token_value only once in order to avoid unnecessary getter call.
+        tok_value = self.token_value
+        if tok_value is not None:
+            authorization_header['Authorization'] = 'Bearer ' + tok_value
         return authorization_header
