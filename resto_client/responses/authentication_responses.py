474f772630c2bbce9c7e6f0c96a57b6367e81080
Alain Giros <Alain.Giros@cnes.fr>
2020-02-17 12:54:53 +0100

    added normalize_response in GetTokenResponse; modified as_resto_object
    as_resto_object now raises AccesDeniedError if credentials were wrong

--- resto_client/responses/authentication_responses.py
+++ resto_client/responses/authentication_responses.py
@@ -22,15 +22,48 @@ from .resto_json_response import RestoJsonResponseSimple
 class GetTokenResponse(RestoJsonResponseSimple):
     """
      Response received from GetTokenRequest.
+
+     The normalized response for a GetToken request is defined by the following fields:
+
+     - 'success': boolean True means that a token was retrieved successfully.
+     - 'token': contains the token string. Exists only when 'success' is True.
+     - 'message': contains the error reason when 'success' is False. Not available for all servers.
     """
     needed_fields: List[str] = []
     optional_fields = ['success', 'token', 'message']
 
+    def normalize_response(self) -> None:
+        """
+        Returns a normalized response whose structure does not depend on the server.
+        """
+        # First copy the fields present in the original response.
+        self._normalized_response = {}
+        if 'success' in self._original_response:
+            self._normalized_response['success'] = self._original_response['success']
+        if 'token' in self._original_response:
+            self._normalized_response['token'] = self._original_response['token']
+        if 'message' in self._original_response:
+            self._normalized_response['message'] = self._original_response['message']
+
+        # Create a 'success' field if it was missing in the original response.
+        if 'success' not in self._normalized_response:
+            # Some servers signal failure by returning an empty token string.
+            # It is also a failure to have no returned token field.
+            self._normalized_response['success'] = self._normalized_response.get('token', '') != ''
+
     def as_resto_object(self) -> str:
         """
         :returns: the response expressed as a Resto object
+        :raises AccesDeniedError: when the GetToken request was rejected for incorrect credentials.
         """
-        return self._normalized_response['token']
+        if self._normalized_response['success']:
+            return self._normalized_response['token']
+        # response reported a failure. If a message is available use it otherwise create one.
+        if 'message' in self._normalized_response:
+            msg_excp = self._normalized_response['message']
+        else:
+            msg_excp = 'Invalid username/password'
+        raise AccesDeniedError(msg_excp)
 
 
 class CheckTokenResponse(RestoJsonResponseSimple):
